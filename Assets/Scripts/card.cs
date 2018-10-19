6ae35dcf6e30fd7e7a1b6b569006cc739b7d677e
NyuSalvatge <albert.viader@gmail.com>
2018-10-19 13:59:00 +0200

    Creacio

---
+++ Assets/Scripts/card.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class card : MonoBehaviour {
+
+    // Use this for initialization
+    public int id = 0;
+	void Start () {
+
+        Debug.Log(id);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
